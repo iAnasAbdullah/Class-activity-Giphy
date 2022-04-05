d946abd98fd263fb3f68dd06c6182c7c02383975
iAnasAbdullah <ianasabdullah@hotmail.com>
2022-04-05 23:20:05 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -3,14 +3,14 @@ function fetchGiphyAPI(keyword) {
     return;
     }
     var url = "https://api.giphy.com/v1/gifs/search";
-    var params = "api_key=Z1G9HDijnRQjfMmD0OYp7UDt8CBz7BWN&limit=5&q=" + encodeURIComponent(keyword);
+    var params = "api_key=Z1G9HDijnRQjfMmD0OYp7UDt8CBz7BWN&limit=5&q=";
     var xhr = new XMLHttpRequest();
     xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4 && this.status === 200) {
     processResponse(this.responseText);
     }
     });
-    xhr.open("GET", url + "?" + params);
+    xhr.open("GET", url + "?" + params + keyword);
     xhr.send();
    }
 
